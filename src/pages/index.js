a870739239d3bec7b937d86a2358ce8d5baaccb5
mastinder singh <mastinder@gmail.com>
2021-07-03 15:46:17 +0530

    Update index.js
    hi

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 import News from "../components/news"
-import stock from "../components/stock"
+import Stock from "../components/stock"
 
 export default () => (
     <Layout>
